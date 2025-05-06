12569e9a3ff531ddd8536754d74604adf6d0e315
dejan.jovelic <dejan.jovelic@gmail.com>
2025-05-06 15:39:36 +0200

    kreirani fajlovi html sa kompletnim kodom, scss i css za neko manje poedsavanje i js sa kodom za klasu i popunjavanje tabele.

---
+++ app.js
@@ -0,0 +1,40 @@
+class Artikl {
+    constructor(naziv, cena, opis) {
+        this.naziv = naziv;
+        this.cena = cena;
+        this.opis = opis;
+    }
+}
+let artikli = [];
+
+function inicijalizacijaArtikla() {
+    artikli = [
+        new Artikl("Monitor", 165, "Asus monitor"),
+        new Artikl("TV", 650, "Samsung TV"),
+        new Artikl("Miš", 20, "Genius miš")
+    ]
+    kreirajRedoveArtikala()
+}
+document.addEventListener("DOMContentLoaded", inicijalizacijaArtikla());
+
+function kreirajRedoveArtikala() {
+    let table = document.querySelector("#artikli-body");
+    table.innerHTML ="";
+
+    for (let i = 0; i < artikli.length; i++) {
+        let tr = document.createElement("tr");
+
+        let br = document.createElement("td");
+        let naziv = document.createElement("td");
+        let cena = document.createElement("td");
+
+        br.textContent = i + 1;
+        naziv.textContent = artikli[i].naziv;
+        cena.textContent = artikli[i].cena;
+
+        tr.appendChild(br);
+        tr.appendChild(naziv);
+        tr.appendChild(cena);
+        table.appendChild(tr)
+    }
+}
